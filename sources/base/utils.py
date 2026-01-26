6499bbf996d20abda43d22cfd64b144f8befdb65
Gaikwad Kunal <162322840+KunalG932@users.noreply.github.com>
2026-01-26 21:24:52 +0530

    Refactor message format constants in utils.py

--- sources/base/utils.py
+++ sources/base/utils.py
@@ -2,8 +2,58 @@
 # If you find any bugs, please let us know in the channel updates.
 # You can 'git pull' to stay updated with the latest changes.
 
-DEAULT_MSG_FORMAT =
-T_MSG_FORMAT =
-ATSU_MSG_FORMAT =
-ALL_MSG_FORMAT =
-BALL_MSG_FORMAT =
+DEAULT_MSG_FORMAT = """<b>{title}</b>
+
+<b>Status:</b> <code>{status}</code>
+<b>Genres:</b> <code>{genres}</code>
+
+<blockquote>{summary}</blockquote>
+
+<b>URL:</b> {url}"""
+
+T_MSG_FORMAT = """<b>{title}</b>
+
+<b>Language:</b> <code>{language}</code>
+<b>Status:</b> <code>{status}</code>
+<b>Genres:</b> <code>{genres}</code>
+
+<blockquote>{summary}</blockquote>
+
+<b>URL:</b> {url}"""
+
+ATSU_MSG_FORMAT = """<b>{title}</b>
+<i>{alt_title}</i>
+
+<b>Type:</b> <code>{type}</code>
+<b>Status:</b> <code>{status}</code>
+<b>Genres:</b> <code>{genres}</code>
+
+<blockquote>{summary}</blockquote>
+
+<b>URL:</b> {url}"""
+
+ALL_MSG_FORMAT = """<b>{title}</b>
+<i>{alt_title}</i>
+
+<b>Type:</b> <code>{type}</code>
+<b>Year:</b> <code>{year}</code>
+<b>Status:</b> <code>{status}</code>
+<b>Genres:</b> <code>{genres}</code>
+
+<blockquote>{summary}</blockquote>
+
+<b>URL:</b> {url}
+<b>AniList:</b> {anilist}
+<b>MAL:</b> {myanime}"""
+
+BALL_MSG_FORMAT = """<b>{title}</b>
+<i>{alt_title}</i>
+
+<b>Year:</b> <code>{year}</code>
+<b>Rating:</b> ⭐ {rating}
+<b>Status:</b> <code>{status}</code>
+<b>Genres:</b> <code>{genres}</code>
+
+<blockquote>{summary}</blockquote>
+
+<b>URL:</b> {url}"""
